e452d23eb40077647f829398e2a446aeb958f96e
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2021-03-12 10:58:16 +0530

    loaded index.html file in browser

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const electron = require("electron");
+
+const { app, BrowserWindow } = electron;
+
+app.on("ready", () => {
+  console.log("App is ready");
+  const mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
