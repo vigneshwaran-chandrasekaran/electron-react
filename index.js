8e7143824fde95632a40df542549c0831f578ffc
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2021-03-12 23:17:30 +0530

    ffmpeg package installed

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const { app, BrowserWindow } = electron;
 
 app.on("ready", () => {
   console.log("App is ready");
-  const mainWindow = new BrowserWindow({});
+
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+    backgroundColor: "#ccc",
+  });
+
   console.log("__dirname", __dirname);
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
