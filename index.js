2e2de800fb5639d801fd8b89c1ab40ff5e47edad
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2021-03-12 22:04:16 +0530

    added input to acces the video file

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const { app, BrowserWindow } = electron;
 app.on("ready", () => {
   console.log("App is ready");
   const mainWindow = new BrowserWindow({});
+  console.log("__dirname", __dirname);
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
