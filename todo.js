b7930dd90810175bf720d662acc824393a6fc833
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2021-03-13 09:26:45 +0530

    todo process started

---
+++ todo.js
@@ -0,0 +1,36 @@
+const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
+
+const { app, BrowserWindow, ipcMain } = electron;
+let mainWindow;
+
+app.on("ready", () => {
+  console.log("App is ready");
+
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+    backgroundColor: "#ccc",
+  });
+
+  console.log("__dirname", __dirname);
+  mainWindow.loadURL(`file://${__dirname}/todo.html`);
+});
+
+ipcMain.on("video:submit", (event, path) => {
+  console.log("path", path);
+  ffmpeg.ffprobe(path, (err, metaData) => {
+    if (err) {
+      console.log("error", err);
+    }
+
+    mainWindow.webContents.send("video:metadata", metaData);
+
+    console.log("metaData", metaData);
+  });
+});
