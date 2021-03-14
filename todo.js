c77583be7237bbdd1949cda6d920c0a3ab0312c4
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2021-03-14 23:14:27 +0530

    add new file new window added

--- todo.js
+++ todo.js
@@ -3,14 +3,48 @@ const ffmpeg = require("fluent-ffmpeg");
 
 const { app, BrowserWindow, Menu } = electron;
 let mainWindow;
+let addWindow;
+
+let ctrl = "Ctrl"; // For windows and Ubuntu
+let platform = process.platform;
+
+if (platform === "darwin") {
+  ctrl = "Command"; // For Mac
+}
+
+console.log("ctrl", ctrl);
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Add New Todo",
+  });
+  addWindow.loadURL(`file://${__dirname}/add.html`);
+}
 
 const menuTemplate = [
   {
     label: "File",
     submenu: [
-      { label: "New Tab" },
+      {
+        label: "New Tab",
+        click() {
+          createAddWindow();
+        },
+      },
+      {
+        label: "Show platform",
+        accelerator: (() => {
+          return `${ctrl}+I`;
+        })(),
+        click() {
+          console.log("Hello " + platform);
+        },
+      },
       {
         label: "Close (வெளியேறு)",
+        accelerator: `${ctrl}+Q`,
         click() {
           app.quit();
         },
@@ -19,7 +53,7 @@ const menuTemplate = [
   },
 ];
 
-if (process.platform === "darwin") {
+if (platform === "darwin") {
   /**
    * On mac os, the file menu will merged into first menu,
    * to avoid that add empty object start of the menus
@@ -41,6 +75,13 @@ app.on("ready", () => {
 
   console.log("__dirname", __dirname);
   mainWindow.loadURL(`file://${__dirname}/todo.html`);
+
+  /**
+   * Close all window when main window is closed
+   */
+  mainWindow.on("closed", () => {
+    app.quit();
+  });
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
